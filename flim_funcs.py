78e986b04273dc52a4daa5424489f607868bf2f6
Zac Swider <zac.swider@gmail.com>
2022-11-29 14:33:18 -0600

    batch analyze folder of asc files

--- flim_funcs.py
+++ flim_funcs.py
@@ -6,6 +6,9 @@ import tkinter as tk
 from scipy.ndimage import convolve
 import matplotlib.pyplot as plt
 from skimage.filters import threshold_otsu
+from tqdm import tqdm
+import pandas as pd
+
 
 def load_sdt(sdt_path, target_channel:int, num_channels:int = 3, im_dim:int = 512, num_bins:int = 256, plot:bool = False) -> np.ndarray:
     ''' 
@@ -31,6 +34,7 @@ def load_sdt(sdt_path, target_channel:int, num_channels:int = 3, im_dim:int = 51
         ax.imshow(np.max(data, axis=2), cmap='gray'); ax.set_title(f'Channel {target_channel}'); ax.axis('off')
         plt.show()
     return data
+
 
 def load_irf(irf_path, target_channel:int, num_channels:int = 3, im_dim:int = 512, num_bins:int = 256, plot:bool = False) -> np.ndarray:
     ''' 
@@ -63,6 +67,7 @@ def load_irf(irf_path, target_channel:int, num_channels:int = 3, im_dim:int = 51
         plt.show()
     return irf_summed
 
+
 def bin_flim_data(data: np.ndarray) -> np.ndarray:
     ''' 
     Spatially bin time-resolved FLIM data to increase curve fitting ability.
@@ -74,13 +79,12 @@ def bin_flim_data(data: np.ndarray) -> np.ndarray:
     -------
     binned_data : numpy.ndarray. The binned data.
     '''
-    if not data.ndim == 3:
-        print("sorry, we're only doing single channels at the moment")
-        return
+    assert data.ndim == 3, "sorry, we're only doing single channels at the moment"
     bin_num = 2
     kernel = np.ones((bin_num, bin_num,1))
     binned = convolve(data, kernel, mode='reflect')
     return binned
+
 
 def load_asc(asc_path) -> np.ndarray:
     ''' 
@@ -94,6 +98,7 @@ def load_asc(asc_path) -> np.ndarray:
     data : numpy.ndarray
     '''
     return np.loadtxt(asc_path)
+
 
 def batch_asc_files_to_tif(in_path = None) -> None:
     ''' 
@@ -110,7 +115,7 @@ def batch_asc_files_to_tif(in_path = None) -> None:
     if in_path is None:
         root = tk.Tk()
         root.withdraw()
-        in_path = tk.filedialog.askdirectory()
+        in_path = tk.filedialog.askdirectory()                      # type: ignore
 
     in_path = Path(in_path)
     file_names = [f.stem for f in in_path.glob('*.asc')]
@@ -145,3 +150,85 @@ def batch_asc_files_to_tif(in_path = None) -> None:
             for i, params_name in enumerate(params):
                 f.write(f'{i} = {params_name}\n')
 
+
+def find_base_file_names(p: Path):
+    """Sort through a folder of .asc files and return a list of the base file names
+    
+    Args
+        p (Path): Path to folder containing .asc files
+    
+    Returns
+        unique_names (list): List of base file names
+    """
+    asc_files = list(p.glob('*.asc'))
+    # remove anything with the word 'phasor' in it
+    asc_files = [x for x in asc_files if 'phasor' not in x.name]
+    base_names = [f.stem.rsplit('_', maxsplit = 1)[0] for f in asc_files]
+    return list(np.unique(base_names))
+
+
+def assign_group_to_names(file_names: list, group_names: dict, assignment: str = 'all') -> dict:
+    """Given a list of file names and dictionary with unique identifiers for each group,
+    assign a group to each file name and return assignments as a dictionary.
+    
+    Args
+        file_names (list): List of file names
+        group_names (dict): Dictionary with group names as keys and unique identifiers as values
+        assignment (str): 'all' or 'any'. If 'all', a file name must contain all unique identifiers,
+        if 'any', a file name may contain any unique identifier.
+    
+    Returns 
+        group_assignments (dict): Dictionary with file names as keys and group names as values
+    """
+    assigned_names = {}
+    for name in file_names:
+        potential_group_matches = []
+        for group_name, identifiers in group_names.items():
+            if assignment == 'all':
+                if any([x not in name for x in identifiers]):
+                    continue
+                potential_group_matches.append(group_name)
+            elif assignment == 'any':
+                if not any([x in name for x in identifiers]):
+                    continue
+                potential_group_matches.append(group_name)
+        if len(potential_group_matches) == 1:
+            assigned_names[name] = potential_group_matches[0]
+        else:
+            print(f'{len(potential_group_matches)} matches found')
+            print(f'Could not assign {name} to a group')
+            assigned_names[name] = 'no group found'
+    return assigned_names
+
+
+def measure_asc_files(base_file_paths: list, 
+                      measurements: list, 
+                      group_assignments: dict) -> pd.DataFrame:
+    """Measure a list of files and return a pandas dataframe with the results
+    
+    Args
+        base_file_paths (list): List of paths to files to be measured. NOTE: these should be full path to base
+        file names, it is assumed that the full path will include a _{meas}.asc at the end.
+        
+    Returns
+        df (pd.DataFrame): Pandas dataframe with results
+    """
+    data_collection = []
+    for full_path in tqdm(base_file_paths):
+        
+        file_name = full_path.stem
+
+        file_measurements = {
+            'file name': file_name,
+            'group': group_assignments[file_name]
+        }
+
+        for measurement in measurements:
+            file_name = f'{full_path}_{measurement}.asc'
+            asc_asarray = load_asc(file_name)
+            file_measurements[f'mean {measurement}'] = asc_asarray.mean()
+            file_measurements[f'median {measurement}'] = np.median(asc_asarray.ravel())
+            file_measurements[f'std {measurement}'] = asc_asarray.std()
+        data_collection.append(file_measurements)
+    
+    return pd.DataFrame(data_collection)
